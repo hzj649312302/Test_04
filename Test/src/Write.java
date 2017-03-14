8cbbbb3837324a023427960aef64f85283221e9d
hzj649312302 <hzj649312302@qq.com>
2017-03-14 11:14:51 +0800

    Test_04

---
+++ Test/src/Write.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Write {
+
+	public static void main(String args[])
+	{
+		int n;
+		Scanner in=new Scanner(System.in);
+		n=in.nextInt();
+		System.out.printf("n^2="+n*n);
+	}
+	
+}
